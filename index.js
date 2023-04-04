febd822c8024dbb80a3a03dbc2a7ec5c042e1628
Cindy Velasquez <ciindvlz@hotmail.com>
2023-04-04 15:34:45 -0400

    MethodLab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+const record = [
+    {year: "2015", result: "W"},
+    {year: "2014", result: "N/A"},
+    {year: "2013", result: "L"},
+]
+
+  function superbowlWin(array) {
+    function determineWin(obj) {
+        if (obj.result === "W") {
+            return obj;
+        }
+    }
+    let win = array.find(determineWin)
+    if (win) {
+        return win.year
+    } else {
+        return undefined;
+    }
+  }
+    
+ 
+    
+  
+
